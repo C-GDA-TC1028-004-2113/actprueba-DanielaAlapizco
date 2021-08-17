7d3136882be5807d71bdd2d8bf77d65f251f76e2
DanielaAlapizco <A01641444@itesm.mx>
2021-08-17 15:01:02 +0000

    Prueba a revisar

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,5 +1,14 @@
 def main():
-    #escribe tu código abajo de esta línea
+    #escribe tu código abajo de esta 
+    x1 = int(input("Dame un número: "))
+    x2 = int(input("Dame un número: "))
+    suma = x1 + x2
+    resta = x1 - x2
+    mult = x1 * x2
+
+    print("Suma: ", suma)
+    print("Resta: ", resta)
+    print("Multiplicación: ", mult)
 
 
 
